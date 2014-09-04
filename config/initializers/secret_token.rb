7c28031f53f19c9f80aabdd3e5d515389cf0983b
sifiso <sifiso@realnet.co.sz>
2014-09-04 11:38:29 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lemur::Application.config.secret_token = '5206ef5b482aee9043b4e656d91958d6f1cb71eced3b8236efe97f147538b6eeaecbfa4a1b508a3a97a71f44a7aeeb5e40e29ad30bb4614f2e438c933e2c8ec0'
